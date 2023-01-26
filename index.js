92fc3ca9d8118378e5ffdc71bc0df82e9cac9293
Pierce Wilson <piercewilson@Pierces-iMac.local>
2023-01-26 16:14:15 -0500

    first stack

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
-console.log("hello")
+const Manager = require("./library/Manager")
+
+
+
+
+const inquirer = require("inquirer")
+var team = []
+
+function userInfo() {
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "name",
+            message: "what is your name?"
+        }
+    ]
+    ).then(function (data){
+        //  console.log(data)
+        const newManager = new Manager(
+            data.name 
+            
+        )
+        //  console.log(newManager)
+        team.push(newManager) 
+        addEmployee()
+    })
+}
+
+
+function addEmployee() {
+    inquirer.prompt([
+        {
+            type: "list",
+            name: "employee",
+            message: "what kind of employee do you want to add?",
+            choices: ["Engineer", "Intern", "Quit"]
+        }
+    ]).then (function (data) {
+        console.log(data)
+        
+    })
+}
+
+userInfo()
